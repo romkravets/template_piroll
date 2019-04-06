86a1e74417e1aed42028b5695798a608e28bcf64
romkravets <romann.kravets@gmail.com>
2019-04-06 20:21:29 +0300

    pablish progect

---
+++ docs/precache-manifest.d00f636d3b7d987790e300eb136988e2.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0fd09e252e724bb523cc68bc147ed3a",
+    "url": "/static/media/pexels.a0fd09e2.jpg"
+  },
+  {
+    "revision": "4e20c5330d8e01747b0d",
+    "url": "/static/css/main.824d8de4.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "36a1f9985fc51a7f646d1de6dbe36475",
+    "url": "/static/media/header.36a1f998.jpeg"
+  },
+  {
+    "revision": "76d3f7fe4c4c189eb72a",
+    "url": "/static/js/2.e33bef0a.chunk.js"
+  },
+  {
+    "revision": "1b4bb2e265225100a3a69b9130b80de1",
+    "url": "/static/media/eye.1b4bb2e2.svg"
+  },
+  {
+    "revision": "b31fb12abd16b2f15932a706fa102c18",
+    "url": "/static/media/milk.b31fb12a.jpg"
+  },
+  {
+    "revision": "ee5f86857b946111be4c72134a55ce73",
+    "url": "/static/media/flyers.ee5f8685.png"
+  },
+  {
+    "revision": "0532e183c129c8654e56e92267909d74",
+    "url": "/static/media/bag.0532e183.jpg"
+  },
+  {
+    "revision": "04c746547c07bca13b5f9352489880eb",
+    "url": "/static/media/phone.04c74654.png"
+  },
+  {
+    "revision": "9fe480f4e96822d54d6c8bac458bc65f",
+    "url": "/static/media/pancil.9fe480f4.jpg"
+  },
+  {
+    "revision": "0260896c05d84254111b7aa3292718ee",
+    "url": "/static/media/book.0260896c.jpg"
+  },
+  {
+    "revision": "6bc3ba5e91f386e19e127845960918ab",
+    "url": "/static/media/she.6bc3ba5e.jpg"
+  },
+  {
+    "revision": "50011290c114f84693647e546aa519a0",
+    "url": "/static/media/honey.50011290.jpg"
+  },
+  {
+    "revision": "a8640299750007fefeb981dd62b33b4c",
+    "url": "/static/media/person.a8640299.mp4"
+  },
+  {
+    "revision": "4e20c5330d8e01747b0d",
+    "url": "/static/js/main.12a75b7b.chunk.js"
+  },
+  {
+    "revision": "6ce6d14558ce6fd3ea6d8ddf2fa3eb21",
+    "url": "/static/media/diamond.6ce6d145.svg"
+  },
+  {
+    "revision": "3818a7ff35027cba078f120eded8e48a",
+    "url": "/static/media/smartphone.3818a7ff.svg"
+  },
+  {
+    "revision": "319efda4022e423402c34eb0f5bafcc4",
+    "url": "/static/media/cupid.319efda4.svg"
+  },
+  {
+    "revision": "c3c2ff1770ac9c87ec43b497a338bb8b",
+    "url": "/static/media/gamepad.c3c2ff17.svg"
+  },
+  {
+    "revision": "171400b6643c55a2624a76eb514fdf0d",
+    "url": "/static/media/aeroplane.171400b6.svg"
+  },
+  {
+    "revision": "1b0495c07b7b8ef23ef275f6296ca973",
+    "url": "/static/media/star.1b0495c0.svg"
+  },
+  {
+    "revision": "e347d51ab17fc5946d48987433876aea",
+    "url": "/static/media/magic-wand.e347d51a.svg"
+  },
+  {
+    "revision": "8be1e5e39938a3783e085b8aee43be1e",
+    "url": "/static/media/paint.8be1e5e3.svg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "88da4d703e03614ec677524c4ebbfd0d",
+    "url": "/static/media/space-office.88da4d70.jpg"
+  },
+  {
+    "revision": "76d3f7fe4c4c189eb72a",
+    "url": "/static/css/2.68eb382a.chunk.css"
+  },
+  {
+    "revision": "1203037c579a438210db39ec296d5bbd",
+    "url": "/index.html"
+  }
+];
